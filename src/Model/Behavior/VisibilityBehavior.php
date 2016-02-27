4bd64c49714d9cfd7c24b4df289ba64b286c6798
Chris Brescia <chris@cwbit.com>
2016-02-27 12:41:35 -0500

    (feat) rebuilt from project

---
+++ src/Model/Behavior/VisibilityBehavior.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Model\Behavior;
+
+use Cake\ORM\Behavior; 				# parent class
+use Cake\ORM\Query;					# Query class needed for findXYZ ops
+
+class VisibilityBehavior extends Behavior{
+
+	/**
+	 * Default configuration settings.
+	 * Can be overridden in `Table->addBehavior('Visiblity', [ .. settings ..])`
+	 */
+	protected $_defaultConfig = [
+		'field' => 'is_visible',	# field used to monitor visibility, e.g. `Ewoks.is_visible`
+		];
+
+	/**
+	 * Filters Query to only show records that are marked visible
+	 * exposed to tables as `$table->find('visible')`
+	 * @param Query $query
+	 * @param array $options
+	 * @return Query
+	 */
+	public function findVisible(Query $query, array $options){
+		# get the merged (possibly customized) config values
+		$config = $this->config();
+
+		# filter the query by visibility
+		return $query->where(["{$this->_table->alias()}.{$config['field']}" => true]);
+	}
+}
